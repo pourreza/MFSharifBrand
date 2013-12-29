718fe3e503e59edb928305b41ffea5c3d519eda1
pourreza <mm.pourreza@gmail.com>
2013-12-29 21:28:13 +0330

    maryam add product ezafe shode

--- Project_Phase2/static/javascript/add_product.js
+++ Project_Phase2/static/javascript/add_product.js
@@ -1,61 +1,61 @@
 
-var url = "http://webproject.roohy.me/ajax/2/m&f/category/list";
-var categry;
-console.log("umad in tu");
-$.ajax({
-    url: url,
-    type: 'post',
-    dataType: 'json',
-    success: function(data, status, xhr){
-        if (data.result == 0){
-            // Request error
-        }else
-        {
-
-            for (var i = 0  ; i < data.categoryList.length ; i++)
-            {
-                var new_li = document.createElement('li');
-                new_li.setAttribute("id", "li_"+data.categoryList[i].id);
-                new_li.setAttribute("class", "li_cat");
-                $("#dropdown_ul").append(new_li);
-                var new_a = document.createElement ('a');
-                new_a.innerHTML = data.categoryList[i].name;
-                new_a.setAttribute("id", data.categoryList[i].id);
-                new_a.setAttribute("name", data.categoryList[i].name);
-                new_a.setAttribute("href", "#");
-                $("#li_"+data.categoryList[i].id).append(new_a);
-                if (i === 1 || i === 0)
-                {
-                    var li_div = document.createElement('li');
-                    li_div.setAttribute("class", "divider");
-                    $("#dropdown_ul").append(li_div);
-                }
-
-
-            }
-
-
-
-            console.log($("li_cat"));
-
-            $('.li_cat').click( function(event){
-                prim_str = event.target.id;
-                console.log("hello world again!");
-                console.log(event.target);
-                console.log(prim_str);
-                category__ = parseInt(prim_str);
-                id_str= prim_str.substring(3);
-                console.log(id_str);
-                document.getElementById("select-button").childNodes[2].nodeValue = event.target.name;
-                categry=event.target.id;
-
-            });
-
-        }
-    },
-
-
-});
+//var url = "http://webproject.roohy.me/ajax/2/m&f/category/list";
+//var categry;
+//console.log("umad in tu");
+//$.ajax({
+//    url: url,
+//    type: 'post',
+//    dataType: 'json',
+//    success: function(data, status, xhr){
+//        if (data.result == 0){
+//            // Request error
+//        }else
+//        {
+//
+//            for (var i = 0  ; i < data.categoryList.length ; i++)
+//            {
+//                var new_li = document.createElement('li');
+//                new_li.setAttribute("id", "li_"+data.categoryList[i].id);
+//                new_li.setAttribute("class", "li_cat");
+//                $("#dropdown_ul").append(new_li);
+//                var new_a = document.createElement ('a');
+//                new_a.innerHTML = data.categoryList[i].name;
+//                new_a.setAttribute("id", data.categoryList[i].id);
+//                new_a.setAttribute("name", data.categoryList[i].name);
+//                new_a.setAttribute("href", "#");
+//                $("#li_"+data.categoryList[i].id).append(new_a);
+//                if (i === 1 || i === 0)
+//                {
+//                    var li_div = document.createElement('li');
+//                    li_div.setAttribute("class", "divider");
+//                    $("#dropdown_ul").append(li_div);
+//                }
+//
+//
+//            }
+//
+//
+//
+//            console.log($("li_cat"));
+//
+//            $('.li_cat').click( function(event){
+//                prim_str = event.target.id;
+//                console.log("hello world again!");
+//                console.log(event.target);
+//                console.log(prim_str);
+//                category__ = parseInt(prim_str);
+//                id_str= prim_str.substring(3);
+//                console.log(id_str);
+//                document.getElementById("select-button").childNodes[2].nodeValue = event.target.name;
+//                categry=event.target.id;
+//
+//            });
+//
+//        }
+//    },
+//
+//
+//});
 
 
 function product_add(){
@@ -134,4 +134,11 @@ function preview(img, selection) {
         marginLeft: '-' + Math.round(scaleX * selection.x1) + 'px',
         marginTop: '-' + Math.round(scaleY * selection.y1) + 'px'
     });
+}
+
+function crop(){
+    alert(document.getElementById("pro-img").value);
+
+    document.getElementById("img-select").src = document.getElementById("pro-img");
+    document.getElementById("img-final").src = document.getElementById("pro-img");
 }
